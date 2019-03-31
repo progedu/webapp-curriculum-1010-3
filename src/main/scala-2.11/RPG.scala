fe659bd90cbd4ce88f65335f0e8e789887283457
SasuraiNoHoge <tanakanoobenjoygame@gmail.com>
2019-03-31 14:49:48 +0900

    防御と回生機能の実装

--- src/main/scala-2.11/RPG.scala
+++ src/main/scala-2.11/RPG.scala
@@ -2,43 +2,69 @@ import java.util.Random
 
 object RPG extends App {
   val random = new Random
-  val monsterCount = 5
-  val hero = new Hero(200, 30)
-  var monsters = for (i <- 1 to monsterCount) yield new Monster(random.nextInt(120), random.nextInt(120), false)
+    val monsterCount = 10
+    val heroHP = 300
+    val hero = new Hero(heroHP, 30,heroHP,0)
+    var monsters = for (i <- 1 to monsterCount) yield new Monster(random.nextInt(120), random.nextInt(120), false)
 
-  println(
-    s"""あなたは冒険中の ${hero} であり、
-        |${monsterCount}匹のモンスターが潜んでいる洞窟を抜けねばならない。
+      println(
+        s"""あなたは冒険中の ${hero} であり、
+       |${monsterCount}匹のモンスターが潜んでいる洞窟を抜けねばならない。
         |【ルール】:
         |1を入力してEnterキーを押すと攻撃、2を入力すると防御、それ以外を入力すると逃走となる。
         |逃走成功確率は50%。逃走に失敗した場合はダメージをうける。
+        ||防御は、敵の攻撃力の半分のダメージをうける。
         |一度でもダメージを受けるとモンスターの体力と攻撃力が判明する。
         |またモンスターを倒した場合、武器を奪いその攻撃力を得ることできる。
         |---------------------------------------------
         |未知のモンスターがあらわれた。""".stripMargin)
 
-  while (!monsters.isEmpty) {
-    val monster = monsters.head
-    val input = scala.io.StdIn.readLine("【選択】: 攻撃[1] or 逃走[0] > ")
+      while (!monsters.isEmpty) {
+        val monster = monsters.head
+        val input = scala.io.StdIn.readLine("【選択】: 攻撃[1] or 防御[2] or 逃走[0] > ")
 
-    if (input == "1") { // 攻撃する
-      hero.attack(monster)
-      println(s"あなたは${hero.attackDamage}のダメージを与え、${monster.attackDamage}のダメージを受けた。")
-    } else { // 逃走する
-      if(hero.escape(monster)) {
-        println("あなたは、モンスターから逃走に成功した。")
-      } else {
-        println(s"あなたは、モンスターから逃走に失敗し、${monster.attackDamage}のダメージを受けた。")
+        if (input == "1") { // 攻撃する
+            hero.attack(monster)
+            println(s"あなたは${hero.attackDamage}のダメージを与え、${monster.attackDamage}のダメージを受けた。")
+
+        } else if (input == "2") { // 防御する
+            hero.defend(monster)
+            println(s"あなたは防御し、${monster.attackDamage / 2}のダメージを受けた。")
+        } else { // 逃走する
+            if(hero.escape(monster)) {
+                println("あなたは、モンスターから逃走に成功した。")
+              } else {
+                println(s"あなたは、モンスターから逃走に失敗し、${monster.attackDamage}のダメージを受けた。")
+              }
+          }
+        println(s"【現在の状態】: ${hero}, ${monster}")
+
+          if (!hero.isAlive) { // Hero が死んでいるかどうか
+            if(!hero.revive()) { //回生するかどうか
+              println(
+                """---------------------------------------------
+                  |【ゲームオーバー】: あなたは無残にも殺されてしまった。 """.stripMargin)
+              System.exit(0)
+            }else {
+              println(s"あなたは無残にも殺されてしまったが復活した。残り、${3-hero.death}回復活可能")
+              //not dead procession
+              notDeadProcessing(monster)
+            }
+          } else {
+            //not dead procession
+            notDeadProcessing(monster)
+          }
       }
-    }
-    println(s"【現在の状態】: ${hero}, ${monster}")
 
-    if (!hero.isAlive) { // Hero が死んでいるかどうか
       println(
-        """---------------------------------------------
-          |【ゲームオーバー】: あなたは無残にも殺されてしまった。 """.stripMargin)
-      System.exit(0)
-    } else if (!monster.isAlive || monster.isAwayFromHero) { // Monster いないかどうか
+        s"""---------------------------------------------
+        |【ゲームクリア】: あなたは困難を乗り越えた。新たな冒険に祝福を。
+        |【結果】: ${hero}""".stripMargin
+        )
+    System.exit(0)
+
+  def notDeadProcessing(monster: Monster): Unit = {
+    if (!monster.isAlive || monster.isAwayFromHero) { // Monster いないかどうか
       if(!monster.isAwayFromHero) { // 倒した場合
         println("モンスターは倒れた。そしてあなたは、モンスターの武器を奪った。")
         if (monster.attackDamage > hero.attackDamage) hero.attackDamage = monster.attackDamage
@@ -52,35 +78,40 @@ object RPG extends App {
       }
     }
   }
-
-  println(
-    s"""---------------------------------------------
-        |【ゲームクリア】: あなたは困難を乗り越えた。新たな冒険に祝福を。
-        |【結果】: ${hero}""".stripMargin
-  )
-  System.exit(0)
-
 }
 
 abstract class Creature(var hitPoint: Int, var attackDamage: Int) {
   def isAlive(): Boolean = this.hitPoint > 0
 }
 
-class Hero(_hitPoint: Int, _attackDamage: Int) extends Creature(_hitPoint, _attackDamage) {
+class Hero(_hitPoint: Int, _attackDamage: Int,var maxHP: Int,var death: Int) extends Creature(_hitPoint, _attackDamage) {
 
-  def attack(monster: Monster): Unit = {
-    monster.hitPoint = monster.hitPoint - this.attackDamage
-    this.hitPoint = this.hitPoint - monster.attackDamage
-  }
+    def attack(monster: Monster): Unit = {
+        monster.hitPoint = monster.hitPoint - this.attackDamage
+        this.hitPoint = this.hitPoint - monster.attackDamage
+      }
 
-  def escape(monster: Monster): Boolean = {
-    val isEscape = RPG.random.nextInt(2) == 1
-    if (!isEscape) {
-      this.hitPoint = this.hitPoint - monster.attackDamage
-    } else {
-      monster.isAwayFromHero = true
+    def defend(monster: Monster): Unit = {
+        this.hitPoint = this.hitPoint - (monster.attackDamage / 2)
+     }
+
+
+      def escape(monster: Monster): Boolean = {
+        val isEscaped = RPG.random.nextInt(2) == 1
+        if (!isEscaped) {
+            this.hitPoint = this.hitPoint - monster.attackDamage
+          } else {
+            monster.isAwayFromHero = true
+          }
+        isEscaped
+      }
+  def revive(): Boolean = {
+    val isRevived = this.death < 3
+    if(isRevived) {
+      this.hitPoint = this.maxHP/2
+      this.death += 1
     }
-    isEscape
+    isRevived
   }
 
   override def toString = s"Hero(体力:${hitPoint}, 攻撃力:${attackDamage})"
@@ -90,6 +121,6 @@ class Hero(_hitPoint: Int, _attackDamage: Int) extends Creature(_hitPoint, _atta
 class Monster(_hitPoint: Int, _attackDamage: Int, var isAwayFromHero: Boolean)
   extends  Creature(_hitPoint, _attackDamage) {
 
-  override def toString = s"Monster(体力: ${hitPoint}, 攻撃力:${attackDamage}, ヒーローから離れている:${isAwayFromHero})"
+  override def toString = s"Monster(体力:${hitPoint}, 攻撃力:${attackDamage}, ヒーローから離れている:${isAwayFromHero})"
 
 }
