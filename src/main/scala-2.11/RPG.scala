41194e643f2784d75cbf039b1be2b734e1538753
yrberrytea <yrberrytea@gmail.com>
2021-02-10 00:20:34 +0900

    ヒーローの振る舞いに魔法を追加"

--- src/main/scala-2.11/RPG.scala
+++ src/main/scala-2.11/RPG.scala
@@ -1,30 +1,35 @@
+import RPG.random
+
 import java.util.Random
 
 object RPG extends App {
   val random = new Random
   val monsterCount = 5
-  val hero = new Hero(200, 30)
+  val hero = new Hero(300, 30)
   var monsters = for (i <- 1 to monsterCount) yield new Monster(random.nextInt(120), random.nextInt(120), false)
 
   println(
     s"""あなたは冒険中の ${hero} であり、
-        |${monsterCount}匹のモンスターが潜んでいる洞窟を抜けねばならない。
-        |【ルール】:
-        |1を入力してEnterキーを押すと攻撃、2を入力すると防御、それ以外を入力すると逃走となる。
-        |逃走成功確率は50%。逃走に失敗した場合はダメージをうける。
-        |一度でもダメージを受けるとモンスターの体力と攻撃力が判明する。
-        |またモンスターを倒した場合、武器を奪いその攻撃力を得ることできる。
-        |---------------------------------------------
-        |未知のモンスターがあらわれた。""".stripMargin)
+       |${monsterCount}匹のモンスターが潜んでいる洞窟を抜けねばならない。
+       |【ルール】:
+       |1を入力してEnterキーを押すと攻撃、それ以外を入力すると逃走となる。
+       |逃走成功確率は50%。逃走に失敗した場合はダメージをうける。
+       |一度でもダメージを受けるとモンスターの体力と攻撃力が判明する。
+       |またモンスターを倒した場合、武器を奪いその攻撃力を得ることできる。
+       |---------------------------------------------
+       |未知のモンスターがあらわれた。""".stripMargin)
 
   while (!monsters.isEmpty) {
     val monster = monsters.head
-    val input = scala.io.StdIn.readLine("【選択】: 攻撃[1] or 逃走[0] > ")
+    val input = scala.io.StdIn.readLine("【選択】: 攻撃[1] or 魔法[2] or 逃走[0] > ")
 
     if (input == "1") { // 攻撃する
       hero.attack(monster)
       println(s"あなたは${hero.attackDamage}のダメージを与え、${monster.attackDamage}のダメージを受けた。")
-    } else { // 逃走する
+    } else if (input == "2") { // 魔法を使う
+      hero.magic(monster)
+    }
+    else { // 逃走する
       if(hero.escape(monster)) {
         println("あなたは、モンスターから逃走に成功した。")
       } else {
@@ -39,7 +44,7 @@ object RPG extends App {
           |【ゲームオーバー】: あなたは無残にも殺されてしまった。 """.stripMargin)
       System.exit(0)
     } else if (!monster.isAlive || monster.isAwayFromHero) { // Monster いないかどうか
-      if(!monster.isAwayFromHero) { // 倒した場合
+      if (!monster.isAwayFromHero) { // 倒した場合
         println("モンスターは倒れた。そしてあなたは、モンスターの武器を奪った。")
         if (monster.attackDamage > hero.attackDamage) hero.attackDamage = monster.attackDamage
       }
@@ -55,8 +60,8 @@ object RPG extends App {
 
   println(
     s"""---------------------------------------------
-        |【ゲームクリア】: あなたは困難を乗り越えた。新たな冒険に祝福を。
-        |【結果】: ${hero}""".stripMargin
+       |【ゲームクリア】: あなたは困難を乗り越えた。新たな冒険に祝福を。
+       |【結果】: ${hero}""".stripMargin
   )
   System.exit(0)
 
@@ -74,13 +79,26 @@ class Hero(_hitPoint: Int, _attackDamage: Int) extends Creature(_hitPoint, _atta
   }
 
   def escape(monster: Monster): Boolean = {
-    val isEscape = RPG.random.nextInt(2) == 1
-    if (!isEscape) {
+    val isEscaped = RPG.random.nextInt(2) == 1
+    if (!isEscaped) {
       this.hitPoint = this.hitPoint - monster.attackDamage
     } else {
       monster.isAwayFromHero = true
     }
-    isEscape
+    isEscaped
+  }
+
+  def magic(monster: Monster): Unit = {
+    val magicDamage = (this.attackDamage * RPG.random.nextDouble).toInt
+    val isStunned = RPG.random.nextInt(3) > 0
+    monster.hitPoint = monster.hitPoint - magicDamage
+    if (isStunned) {
+      println(s"あなたは${magicDamage}のダメージを与えた。")
+      println("モンスターは硬直して動けない。")
+    } else {
+      this.hitPoint = this.hitPoint - monster.attackDamage
+      println(s"あなたは${magicDamage}のダメージを与え、${monster.attackDamage}のダメージを受けた。")
+    }
   }
 
   override def toString = s"Hero(体力:${hitPoint}, 攻撃力:${attackDamage})"
@@ -90,6 +108,6 @@ class Hero(_hitPoint: Int, _attackDamage: Int) extends Creature(_hitPoint, _atta
 class Monster(_hitPoint: Int, _attackDamage: Int, var isAwayFromHero: Boolean)
   extends  Creature(_hitPoint, _attackDamage) {
 
-  override def toString = s"Monster(体力: ${hitPoint}, 攻撃力:${attackDamage}, ヒーローから離れている:${isAwayFromHero})"
+  override def toString = s"Monster(体力:${hitPoint}, 攻撃力:${attackDamage}, ヒーローから離れている:${isAwayFromHero})"
 
 }
